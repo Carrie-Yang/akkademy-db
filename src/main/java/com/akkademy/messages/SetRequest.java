41ee893bd16ec6114404aef5c358f4dbfefdae5c
Carrie Yang <carrieyang12@126.com>
2016-12-30 18:05:24 +0800

    AkkademyDb synchronous test

---
+++ src/main/java/com/akkademy/messages/SetRequest.java
@@ -0,0 +1,22 @@
+package com.akkademy.messages;
+
+/**
+ * Created by yanglu on 2016/12/28.
+ */
+public class SetRequest {
+    private final String key;
+    private final Object value;
+
+    public SetRequest(String key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
